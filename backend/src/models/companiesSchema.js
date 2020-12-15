cb9005242381cf0c61b342d06b63e9742b2c0167
botelholarissa <conta.larissa@gmail.com>
2020-12-15 12:09:39 -0300

    fix: companiesSchema

--- backend/src/models/companiesSchema.js
+++ backend/src/models/companiesSchema.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const companySchema = new mongoose.Schema({
     _id: {
         type: mongoose.Schema.Types.ObjectId,
+        auto: true,
         required: true
     },
     name: {
