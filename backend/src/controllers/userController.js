7b14f37741dd5db5202828384856383f0166e218
botelholarissa <conta.larissa@gmail.com>
2020-12-15 10:55:30 -0300

    add user CRUD

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -9,6 +9,74 @@ const getAll = (req, res) => {
     })
 }
 
+const getById = (req, res) => {
+    id = req.params.id;
+
+    usersCollection.findById(id, (error, user) => {
+        if(error)
+            return res.status(500).send(error);
+        else{
+            if(user !== null)
+                return res.status(200).send(user);
+            else
+                return res.status(404).send({ message: "Usuário não existe na base de dados."});
+        }
+    })
+}
+
+const addUser = (req, res) => {
+    const userBody = req.body;
+    const user = new usersCollection(userBody);
+
+    user.save((error) => {
+        if(error)
+            return res.status(400).send(error);
+        else 
+            return res.status(200).send(user)
+    })
+}
+
+const updateUser = (req, res) => {
+    const id = req.query.id;
+    const userBody = req.body;
+    const update = { new: true };
+
+    usersCollection.findByIdAndUpdate(
+        id,
+        userBody,
+        update,
+        (error, user) => {
+            if(error)
+                return res.status(500).send(error);
+            else {
+                if(user)
+                    return res.status(200).send(user); 
+                else 
+                    return res.status(404).send({ message: "Usuário não existe na base de dados."});
+            }
+        })
+}
+
+const deleteUser = (req, res) => {
+    const id = req.query.id;
+
+    usersCollection.findByIdAndDelete(id, (error, user) => {
+        console.log(user)
+        if(error)
+            return res.status(500).send(error);
+        else {
+            if(user)
+                return res.status(200).send({ message: "Usuário deletado"})
+            else
+                return res.status(404).send({ message: "Usuário não existe na base de dados."})
+        }
+    })
+}
+
 module.exports = {
-    getAll
+    getAll,
+    getById,
+    addUser,
+    updateUser,
+    deleteUser
 }
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const controller = require('../controllers/userController');
 
 router.get('/', controller.getAll);
+router.get('/:id', controller.getById);
+router.post('/create', controller.addUser);
+router.put('/update', controller.updateUser);
+router.delete('/delete', controller.deleteUser);
 
 module.exports = router;
